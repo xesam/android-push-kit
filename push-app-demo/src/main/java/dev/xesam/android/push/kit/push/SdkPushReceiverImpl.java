08739913ac762f0ff61645cd6c40871ba59803a6
xesam <xesamguo@gmail.com>
2017-04-18 12:05:04 +0800

    third

--- push-app-demo/src/main/java/dev/xesam/android/push/kit/push/SdkPushReceiverImpl.java
+++ push-app-demo/src/main/java/dev/xesam/android/push/kit/push/SdkPushReceiverImpl.java
@@ -2,7 +2,9 @@ package dev.xesam.android.push.kit.push;
 
 import android.content.Context;
 
+import dev.xesam.android.push.kit.api.PushMsg;
 import dev.xesam.android.push.kit.api.PushRaw;
+import dev.xesam.android.push.kit.api.PushToken;
 import dev.xesam.chelaile.push.spi.AbsSdkPushReceiver;
 
 /**
@@ -13,15 +15,23 @@ public class SdkPushReceiverImpl extends AbsSdkPushReceiver {
 
     @Override
     public boolean checkPushRaw(Context context, PushRaw data) {
-        return false;
+        return true;
     }
 
     @Override
     public void onReceive(Context context, PushRaw raw) {
         if (raw.getType() == PushRaw.TYPE_TOKEN) {
-
+            onReceiveToken((PushToken) raw);
         } else if (raw.getType() == PushRaw.TYPE_MSG) {
-
+            onReceiveMsg((PushMsg) raw);
         }
+    }
+
+    private void onReceiveToken(PushToken token) {
+        // TODO: 17-4-18
+    }
+
+    private void onReceiveMsg(PushMsg msg) {
+        // TODO: 17-4-18
     }
 }
