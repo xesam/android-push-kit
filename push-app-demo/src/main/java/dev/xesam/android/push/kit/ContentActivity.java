3ab9f62f2a35fe0ab0d452be4c4bc90fea3e60ac
xesam <xesamguo@gmail.com>
2017-04-18 12:07:03 +0800

    ContentActivity

--- push-app-demo/src/main/java/dev/xesam/android/push/kit/ContentActivity.java
+++ push-app-demo/src/main/java/dev/xesam/android/push/kit/ContentActivity.java
@@ -1,13 +1,35 @@
 package dev.xesam.android.push.kit;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import dev.xesam.android.push.kit.push.AppPushReceiverImpl;
 
 public class ContentActivity extends AppCompatActivity {
+
+    public static final String TAG = "ContentActivity";
+
+    private AppPushReceiverImpl appPushReceiver = new AppPushReceiverImpl() {
+        @Override
+        protected boolean onHandleReceive(Context context, Intent intent) {
+            Log.d(TAG, "onHandleReceive");
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
+        appPushReceiver.register(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        appPushReceiver.unregister(this);
     }
 }
