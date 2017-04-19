c68519e30d431fc1c5d0434b75caf95780f07239
xesam <xesamguo@gmail.com>
2017-04-19 17:30:27 +0800

    mock

--- push-source-mock/src/main/java/dev/xesam/chelaile/push/spi/AbsSdkPushReceiver.java
+++ push-source-mock/src/main/java/dev/xesam/chelaile/push/spi/AbsSdkPushReceiver.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 
 import dev.xesam.android.push.kit.api.CoreSdkPushReceiver;
+import dev.xesam.android.push.kit.api.PushHelper;
 
 /**
  * 个推透传广播接收
@@ -11,6 +12,13 @@ import dev.xesam.android.push.kit.api.CoreSdkPushReceiver;
 public abstract class AbsSdkPushReceiver extends CoreSdkPushReceiver {
     @Override
     public final void onReceive(Context context, Intent intent) {
+        PushHelper.checkReceiveSdk(context, intent, this);
+//        final String action = intent.getAction();
+//        if (PushHelper.getPackagePushConfig(context, Constant.ACTION_TOKEN).equals(action)) {
+//            PushHelper.broadcastSdk();
+//        } else if (PushHelper.getPackagePushConfig(context, Constant.ACTION_MESSAGE).equals(action)) {
+//
+//        }
     }
 
 }
---
+++ push-source-mock/src/main/java/dev/xesam/chelaile/push/spi/Constant.java
@@ -0,0 +1,10 @@
+package dev.xesam.chelaile.push.spi;
+
+/**
+ * Created by xesamguo@gmail.com on 17-4-19.
+ */
+
+class Constant {
+    public static final String ACTION_TOKEN = "ACTION_TOKEN";
+    public static final String ACTION_MESSAGE = "ACTION_MESSAGE";
+}
