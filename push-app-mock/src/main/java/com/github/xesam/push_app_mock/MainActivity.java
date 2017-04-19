a8390bf82df428cfe245f857cb4f9631bdc3c12a
xesam <xesamguo@gmail.com>
2017-04-19 17:38:20 +0800

    mock

--- push-app-mock/src/main/java/com/github/xesam/push_app_mock/MainActivity.java
+++ push-app-mock/src/main/java/com/github/xesam/push_app_mock/MainActivity.java
@@ -1,7 +1,13 @@
 package com.github.xesam.push_app_mock;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import dev.xesam.android.push.kit.api.PushHelper;
+import dev.xesam.android.push.kit.api.PushRaw;
+import dev.xesam.android.push.kit.api.PushToken;
+import dev.xesam.android.push.kit.api.SimplePushMsg;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.send_token).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PushRaw pushRaw = new PushToken("a-mock-token");
+                PushHelper.broadcastSdk(getApplicationContext(), pushRaw);
+            }
+        });
+
+        findViewById(R.id.send_notice_a).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PushRaw pushRaw = new SimplePushMsg("{\"type\":\"a\",\"name\":\"noticeA\"}");
+                PushHelper.broadcastSdk(getApplicationContext(), pushRaw);
+            }
+        });
+
+        findViewById(R.id.send_notice_b).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PushRaw pushRaw = new SimplePushMsg("{\"type\":\"b\",\"name\":\"noticeB\"}");
+                PushHelper.broadcastSdk(getApplicationContext(), pushRaw);
+            }
+        });
+
+        findViewById(R.id.send_notice_c).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PushRaw pushRaw = new SimplePushMsg("{\"type\":\"c\",\"name\":\"noticeC\"}");
+                PushHelper.broadcastSdk(getApplicationContext(), pushRaw);
+            }
+        });
     }
 }
